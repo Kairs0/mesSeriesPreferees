83021a4dd1745f34ca34eb0e2de62e0c0a73c370
BastianMasson <bastian.masson@orange.fr>
2017-10-20 10:00:45 +0200

    Add génération page série

--- Series/MainPage.xaml.cs
+++ Series/MainPage.xaml.cs
@@ -154,7 +154,7 @@ namespace Series
 
         private void ClickBouttonNav(object sender, RoutedEventArgs e)
         {
-            string NomSerie = "NomSerie donné par MainPage";
+            string NomSerie = "Friends";
             this.Frame.Navigate(typeof(DetailsSerie), NomSerie);
         }
     }
