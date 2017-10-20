5850bee6aa82a539eea2081e3b5859140cf70b1c
Kairs0 <arnaud.convers@gmail.com>
2017-10-20 10:34:49 +0200

    add search cast by id show

---
+++ Series/Models/BindPersonToCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series.Models
+{
+    class BindPersonToCharacter
+    {
+        public BindPersonToCharacter(People person, People character)
+        {
+            Person = person;
+            Character = character;
+        }
+
+        public People Person { get; }
+        public People Character { get; }
+    }
+}
