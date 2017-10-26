3dbfb8889e460bdce0dac1b6432acdb54fca2f69
Kairs0 <arnaud.convers@gmail.com>
2017-10-26 16:01:56 +0200

    add interface to manage favorites

--- Series/Favoris.cs
+++ Series/Favoris.cs
@@ -10,36 +10,79 @@ namespace Series
 {
     public static class Favoris
     {
-        public static string CheminSauvegarde = System.IO.Directory.GetCurrentDirectory() + "Sauvegardes\\favoris.txt";
+        private static string NameFavoriteFile = "favoris.txt";
+        private static Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
 
-        //TODO
+        //Rajoute le favoris dans le fichier de favoris (présent dans le dossier AppData de l'user)
         public static void AddToFavorite(string idSerie)
         {
-            using (StreamWriter sw = File.AppendText(CheminSauvegarde))
-            {
-                sw.Write(idSerie + ";");
-            }
-            //if (File.Exists(CheminSauvegarde))
-            //{
-            //    using (StreamWriter sw = File.AppendText(CheminSauvegarde))
-            //    {
-            //        sw.Write(idSerie + ";");
-            //    }
-            //}
+            var existingList = GetFavoritesId();
+            existingList.Add(idSerie);
+            WriteListOfFavorites(existingList);
         }
 
-        //TODO
-        public static void RemoveFavorite(string idSerie)
+        //Supprime le favoris dans le fichier de favoris
+        public static void RemoveFromFavorite(string idSerie)
         {
-            
+            var existingList = GetFavoritesId();
+            existingList.Remove(idSerie);
+            WriteListOfFavorites(existingList);
         }
 
-        //TODO
-        public static List<Serie> GetFavorites(string idSerie)
+        //Retourne la liste des favoris de l'user sous forme d'une liste de séries
+        public static List<Serie> GetFavorites()
         {
             var result = new List<Serie>();
+            var seriesId = GetFavoritesId();
+            foreach (var id in seriesId)
+            {
+                var serie = Api.GetShowById(id);
+                result.Add(serie);
+            }
+
             return result;
         }
 
+        private static List<string> GetFavoritesId()
+        {
+            string content = string.Empty;
+            Windows.Storage.StorageFile favoriteFile;
+            Task.Run(async () =>
+            {
+                try
+                {
+                    favoriteFile = await storageFolder.GetFileAsync(NameFavoriteFile);
+                    content = await Windows.Storage.FileIO.ReadTextAsync(favoriteFile);
+                }
+                catch (FileNotFoundException) { }
+            }).GetAwaiter().GetResult();
+
+            if (content != String.Empty)
+            {
+                return content.Split(';').ToList();
+            }
+            
+            return new List<string>();
+        }
+
+
+        private static void WriteListOfFavorites(List<string> ids)
+        {
+            var stringToWrite = String.Join(";", ids);
+            Task.Run(async () =>
+            {
+                Windows.Storage.StorageFile favoriteFile;
+                try
+                {
+                    favoriteFile = await storageFolder.GetFileAsync(NameFavoriteFile);
+                }
+                catch (FileNotFoundException)
+                {
+                    favoriteFile = await storageFolder.CreateFileAsync(NameFavoriteFile,
+                        Windows.Storage.CreationCollisionOption.ReplaceExisting);
+                }
+                await Windows.Storage.FileIO.WriteTextAsync(favoriteFile, stringToWrite);
+            }).GetAwaiter().GetResult();
+        }
     }
 }
